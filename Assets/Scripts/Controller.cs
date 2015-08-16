b3450500dfd2d170ea26239cd2997119eaf810e1
Max <maxroth52@gmail.com>
2015-08-15 23:15:44 -0500

    Added UI

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+    public int Cash, AP, Food, MP, Year, Workers, Fighters, Scouts, Children,
+               Farm, School, Armory, ScoutSchool;
+
+    public bool buyWorker, buyFighter, makeChild, buildFarm, doScavage, doEducate,
+                buildMenu, actionMenu, buildArmory, buildScoutSchool;
+
+
+	// Use this for initialization
+	void Start () {
+        Cash = 1000;
+        
+	 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/sUIUpdater.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class sUIUpdater : MonoBehaviour {
+
+
+    public Text CashScore, APScore, FoodScore, MPScore,
+                YearScore, WorkerScore, ScoutScore,
+                ChildrenScore, FarmScore, ArmoryScore;
+    public Controller scores;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        CashScore.text = scores.Cash.ToString();
+	
+	}
+}
